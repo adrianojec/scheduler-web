e460a14b3b51d8c951d8d1c7b34e5afbc14799eb
adrianojec <adrianojeric.28@gmail.com>
2023-05-06 22:12:59 +0800

    initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { store } from './app/store/store';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './app/layout/styles.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
