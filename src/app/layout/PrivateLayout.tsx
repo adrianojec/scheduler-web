7b0134a292c99abc57a8de3166cde8c5c59eac2d
adrianojec <adrianojeric.28@gmail.com>
2023-05-16 22:53:07 +0800

    add top navbar

--- src/app/layout/PrivateLayout.tsx
+++ src/app/layout/PrivateLayout.tsx
@@ -1,12 +1,26 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Navbar, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import SideNav from "../components/SideNav/SideNav";
-import { navigationPaths } from "../utilities/constants";
+import { SCHEDULER, SIGN_IN_AS, navigationPaths } from "../utilities/constants";
 
-const PrivateLayout = () => {
+interface Props {
+  userName: string;
+}
+
+const PrivateLayout = ({ userName }: Props) => {
   return (
     <div className="vh-100">
       <Row className="h-100">
+        <Navbar>
+          <div className="w-100 center-items me-5">
+            <span className="text-icon">{SCHEDULER}</span>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>{SIGN_IN_AS(userName)}</Navbar.Text>
+            </Navbar.Collapse>
+          </div>
+        </Navbar>
+
         <Col className="sidenav">
           <SideNav navigationPaths={navigationPaths} />
         </Col>
--- src/app/layout/PrivateRoute.tsx
+++ src/app/layout/PrivateRoute.tsx
@@ -1,12 +1,12 @@
 import { Navigate } from "react-router-dom";
-import { useAppSelector } from "../store/hooks"
+import { useAppSelector } from "../store/hooks";
 import { ROUTE } from "../utilities/enums";
 import PrivateLayout from "./PrivateLayout";
 
 const PrivateRoute = () => {
-    const { user } = useAppSelector(state => state.user);
+  const { user } = useAppSelector((state) => state.user);
 
-    return !!user ? <PrivateLayout /> : <Navigate to={ROUTE.LOGIN} />
-}
+  return !!user ? <PrivateLayout userName={user.userName} /> : <Navigate to={ROUTE.LOGIN} />;
+};
 
-export default PrivateRoute
+export default PrivateRoute;
